7e56f4f563b8d04e4309f1c9c55db81982bbafd7
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2023-02-14 16:36:43 -0400

    d

---
+++ learn/src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter , Route , Switch } from 'react-router-dom'
+
+export const Routes = () =>{
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Route path='/' component={() => <h1>Olá</h1>} />
+            </Switch>
+        </BrowserRouter>
+    );
+}
